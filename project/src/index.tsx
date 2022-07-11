4a840830011581fa95ece0dfbb2bc5cbf9b85237
ma3atr0n <38848459+ma3atr0n@users.noreply.github.com>
2022-07-11 15:27:26 +0300

    2.9. Начинаем программировать

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -14,8 +14,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App 
-      filmCard = {filmCard} 
+    <App
+      filmCard = {filmCard}
     />
   </React.StrictMode>,
 );
