0aa3ba733a3867564082a175187b5f6853818584
ma3atr0n <38848459+ma3atr0n@users.noreply.github.com>
2022-07-11 15:37:03 +0300

    2.9. Начинаем программировать

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,27 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
-<<<<<<< HEAD
 const filmCard = {
   title: 'The Grand Budapest Hotel',
   genre: 'Drama',
   year: 2014
 };
 
-=======
->>>>>>> master
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <App
       filmCard = {filmCard}
     />
-=======
-    <App />
->>>>>>> master
   </React.StrictMode>,
 );
